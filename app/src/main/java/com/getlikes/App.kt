0097fef6e9036617b956823e6a1faa8e02d4b144
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-03-08 19:02:32 +0300

    Fixed codestyle

--- app/src/main/java/com/getlikes/App.kt
+++ app/src/main/java/com/getlikes/App.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
 
-
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
