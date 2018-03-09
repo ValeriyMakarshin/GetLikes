f5330cc14519a6359355768aee9ab59a119d2b1b
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-03-09 18:54:54 +0300

    Fixed fragment_earncoins

--- app/src/main/java/com/getlikes/core/BaseFragment.kt
+++ app/src/main/java/com/getlikes/core/BaseFragment.kt
@@ -1,9 +1,18 @@
 package com.getlikes.core
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import me.dmdev.rxpm.base.PmSupportFragment
+
 
 abstract class BaseFragment<PM : BasePresentationModel> : PmSupportFragment<PM>() {
     abstract val activityInfo: ActivityInfo
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+        inflater.inflate(activityInfo.layoutId, container, false)
 
     override fun onBindPresentationModel(pm: PM) {
 
