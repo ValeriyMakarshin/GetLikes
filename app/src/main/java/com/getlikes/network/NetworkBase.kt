18cf4fc9ab2f54147763eaf0c3ab4b944cc2a18a
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-03-22 15:26:47 +0300

    Added temp interceptor

--- app/src/main/java/com/getlikes/network/NetworkBase.kt
+++ app/src/main/java/com/getlikes/network/NetworkBase.kt
@@ -1,12 +1,16 @@
 package com.getlikes.network
 
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkBase {
     const val BASE_URL = "https://getlikesapp.net/api/"
+    const val SALT = "7dV~US"
+    const val NAME_SIGNATURE = "signature"
+
 
     fun getApi(): Api =
         Retrofit.Builder()
@@ -21,6 +25,24 @@ object NetworkBase {
         val builder = OkHttpClient.Builder()
         NetworkUtils.addStethoInterceptor(builder)
 
+        builder.addInterceptor { chain ->
+            var request: Request = chain.request()
+
+            val requestUrl = request.url()
+            val hash = sortedSetOf(requestUrl.queryParameterNames())
+                .map {
+                    { name: String -> requestUrl.queryParameterValues(name) }
+                }
+                .joinToString().plus(SALT)
+
+            request = request.newBuilder()
+                .url(request.url()
+                    .newBuilder()
+                    .addQueryParameter(NAME_SIGNATURE, hash)
+                    .build())
+                .build()
+            chain.proceed(request)
+        }
         return builder.build()
     }
 
