9aed5d6648d1189f5dde7cfd714f714cf5110b11
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-03-29 18:42:37 +0300

    Moved init base kodein

--- app/src/main/java/com/getlikes/App.kt
+++ app/src/main/java/com/getlikes/App.kt
@@ -3,12 +3,8 @@ package com.getlikes
 import android.app.Application
 import com.crashlytics.android.Crashlytics
 import com.getlikes.di.AppKodein
-import com.getlikes.login.di.LoginKodein
-import com.getlikes.main.di.MainKodein
 import com.getlikes.network.NetworkUtils
-import com.getlikes.splash.di.SplashKodein
 import com.getlikes.util.storage.Storage
-import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.KodeinAware
 import com.github.salomonbrys.kodein.instance
 import io.fabric.sdk.android.Fabric
@@ -18,12 +14,7 @@ class App : Application(), KodeinAware {
         lateinit var INSTANCE: App
     }
 
-    override val kodein = Kodein {
-        import(AppKodein.initModule())
-        import(SplashKodein.initModule())
-        import(LoginKodein.initModule())
-        import(MainKodein.initModule())
-    }
+    override val kodein = AppKodein.baseKodein()
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/getlikes/di/AppKodein.kt
+++ app/src/main/java/com/getlikes/di/AppKodein.kt
@@ -2,8 +2,11 @@ package com.getlikes.di
 
 import android.content.Context
 import com.getlikes.App
+import com.getlikes.login.di.LoginKodein
+import com.getlikes.main.di.MainKodein
 import com.getlikes.network.Api
 import com.getlikes.network.NetworkBase
+import com.getlikes.splash.di.SplashKodein
 import com.getlikes.util.Strings
 import com.getlikes.util.TokenHolder
 import com.getlikes.util.storage.Storage
@@ -17,6 +20,14 @@ import dev.niekirk.com.instagram4android.Instagram4Android
 
 class AppKodein {
     companion object {
+        fun baseKodein() = Kodein {
+            import(AppKodein.initModule())
+            import(SplashKodein.initModule())
+            import(LoginKodein.initModule())
+            import(MainKodein.initModule())
+        }
+
+
         fun initModule() = Kodein.Module {
             bind<App>() with singleton { App.INSTANCE }
 
