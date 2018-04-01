eaefafe9d567d8ee1d524f3a118791fce3a7ad69
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-04-01 20:43:39 +0300

    Fixed task model

--- app/src/main/java/com/getlikes/model/Task.kt
+++ app/src/main/java/com/getlikes/model/Task.kt
@@ -1,10 +1,13 @@
 package com.getlikes.model
 
-import com.getlikes.util.Strings
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 class Task(
     @Expose @SerializedName("media_id")
-    val mediaId: String = Strings.EMPTY
+    val mediaId: String,
+    @Expose @SerializedName("thumbnail_url")
+    val thumbnailUrl: String,
+    @Expose @SerializedName("standard_resolution_url")
+    val standardResolutionUrl: String
 ) : Data
