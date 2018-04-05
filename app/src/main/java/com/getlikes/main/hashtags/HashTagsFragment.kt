1208a5b8fd012c35c2b32d0550226bc78a6564cf
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-04-05 14:58:54 +0300

    Added recycler view

--- app/src/main/java/com/getlikes/main/hashtags/HashTagsFragment.kt
+++ app/src/main/java/com/getlikes/main/hashtags/HashTagsFragment.kt
@@ -7,6 +7,7 @@ import com.github.salomonbrys.kodein.instance
 
 class HashTagsFragment : BaseFragment<HashTagsContract.View, HashTagsContract.Presenter>(),
     HashTagsContract.View {
+
     override val presenter: HashTagsContract.Presenter by instance()
 
     override val activityInfo: ActivityInfo = ActivityInfo(R.layout.fragment_hashtags)
