b0bb9d81626e2969047c8036658c9e01793ff138
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-04-12 18:39:02 +0300

    Added set text for categories

--- app/src/main/java/com/getlikes/adapter/CategoriesAdapter.kt
+++ app/src/main/java/com/getlikes/adapter/CategoriesAdapter.kt
@@ -5,6 +5,4 @@ import com.getlikes.core.BaseRAdapter
 import com.getlikes.model.Category
 
 class CategoriesAdapter(array: Array<Category>) :
-    BaseRAdapter<Category, CategoriesHolder>(array, CategoriesHolder.creator()) {
-
-}
+    BaseRAdapter<Category, CategoriesHolder>(array, CategoriesHolder.creator())
--- app/src/main/java/com/getlikes/adapter/holder/CategoriesHolder.kt
+++ app/src/main/java/com/getlikes/adapter/holder/CategoriesHolder.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import com.getlikes.R
 import com.getlikes.core.BaseHolder
 import com.getlikes.model.Category
+import kotlinx.android.synthetic.main.item_category.view.*
 
 class CategoriesHolder(itemView: View) : BaseHolder<Category>(itemView) {
     companion object {
@@ -17,6 +18,7 @@ class CategoriesHolder(itemView: View) : BaseHolder<Category>(itemView) {
     }
 
     override fun onDraw(item: Category) {
+        itemView.uiNameTv.text = item.name
     }
 
 }
